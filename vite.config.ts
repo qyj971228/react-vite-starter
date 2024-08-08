4d73bf64e8535a2abb3d7b692b11e85e99b10567
qyj <1078869122@qq.com>
2024-08-08 18:33:05 +0800

    feat: vite serve port

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3333,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-})
+});
