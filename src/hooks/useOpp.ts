5137818bfe0ddda9228193b73105749c0b6d91ba
qyj <1078869122@qq.com>
2024-08-12 10:49:37 +0800

    feat: useOpp

---
+++ src/hooks/useOpp.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+type UseOppResult<T, K> = [T | K | boolean, () => void, () => void, () => void];
+
+function useOpp<T = false, K = true>(status?: [T, K]): UseOppResult<T, K> {
+  const DEFAULT: T | false = status ? status[0] : false;
+  const OPPOSITE: K | true = status ? status[1] : true;
+
+  const [state, setState] = useState<T | K | boolean>(DEFAULT);
+
+  function opposite() {
+    setState(state === DEFAULT ? OPPOSITE : DEFAULT);
+  }
+
+  function recover() {
+    setState(DEFAULT);
+  }
+
+  function reverse() {
+    setState(OPPOSITE);
+  }
+
+  return [state, recover, reverse, opposite];
+}
+
+export default useOpp;
